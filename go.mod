e4bdac9235db00ccfb1c87e272e65f6200912a66
Евгений Князев <evgeniy.knyazev@eyevox.ru>
2022-12-28 19:37:23 +0300

    Example: sync goroutines uses channels

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/RndIT/go_demos
+
+go 1.18
---
+++ main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+)
+
+func greet(c chan string, cntrol chan int) {
+	fmt.Println("Init gret")
+	for {
+		select {
+		case i := <-c:
+			{
+				fmt.Println("Hello " + i + "!")
+				if len(c) == 0 {
+					cntrol <- 0
+				}
+			}
+
+		}
+	}
+}
+
+func main() {
+	var names = [...]string{"A", "b", "c", "d", "e"}
+
+	fmt.Println("main() started")
+	c := make(chan string, 2)
+	control := make(chan int)
+	go greet(c, control)
+	defer close(c)
+	defer close(control)
+
+	cnt := 0
+	for idx, item := range names {
+
+		fmt.Println("+ Add to chain item ", item)
+		c <- item
+		cnt++
+		fmt.Printf("= add done %s. len chain=%d\n", item, cnt)
+		if cnt == 2 || idx == len(names)-1 {
+			select {
+			case k := <-control:
+				{
+					fmt.Println("UnLock main gorutine", k)
+					cnt = 0
+				}
+			}
+		}
+	}
+
+	fmt.Println("main() stopped")
+}
