1489734b39aa6d44c40f1b09113cd2b4052deb53
Евгений Князев <evgeniy.knyazev@eyevox.ru>
2023-01-18 13:20:02 +0300

    add tests for functions

--- internal/domain/domain_test.go
+++ internal/domain/domain_test.go
@@ -2,7 +2,9 @@
 
 package domain
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestOrder_value(t *testing.T) {
 	tests := []struct {
@@ -59,6 +61,63 @@ func TestOrder_value(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := tt.order.value(); got != tt.want {
 				t.Errorf("Order.value() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestOrder_Add(t *testing.T) {
+	type args struct {
+		item Item
+	}
+
+	type TestOrder struct {
+		name    string
+		order   *Order
+		args    args
+		wantErr bool
+	}
+
+	var tests []TestOrder
+	order := new(Order)
+	order.Id = 1
+	order.Customer.Id = 1
+	order.Customer.Name = "CName"
+	// сначала пустой ордерс-лист
+	order.Items = []Item{}
+
+	tests = append(tests, TestOrder{
+		name:    "In order 100$. Normal state.",
+		order:   order,
+		args:    args{Item{1, "Item 100$", 100.0, true}},
+		wantErr: false,
+	})
+
+	tests = append(tests, TestOrder{
+		name:    "Add to order 100$ item. Normal state.",
+		order:   order,
+		args:    args{Item{1, "Item 100$", 100.0, true}},
+		wantErr: false,
+	})
+
+	tests = append(tests, TestOrder{
+		name:    "Add to order 50$ item. Normal state.",
+		order:   order,
+		args:    args{Item{1, "Item 50$", 50.0, true}},
+		wantErr: false,
+	})
+
+	tests = append(tests, TestOrder{
+		name:    "Add to order 1$ item. Failed state.",
+		order:   order,
+		args:    args{Item{1, "Item 1$", 1.0, true}},
+		wantErr: true,
+	})
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := tt.order.Add(tt.args.item); (err != nil) != tt.wantErr {
+				t.Errorf("Order.Add() error = %v, wantErr %v", err, tt.wantErr)
 			}
 		})
 	}
